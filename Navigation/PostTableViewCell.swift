85c73b68b2015cefb4e145667384c8554f59fd9e
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-03 17:56:33 +0200

    cell...TextView...

--- Navigation/PostTableViewCell.swift
+++ Navigation/PostTableViewCell.swift
@@ -32,20 +32,27 @@ class PostTableViewCell: UITableViewCell {
     var descriptionText: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        //label.backgroundColor = .cyan
-        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = UIColor.systemGray
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     var likesLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     var viewsLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -62,7 +69,7 @@ class PostTableViewCell: UITableViewCell {
     private let hStack: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        
+        stackView.backgroundColor = .cyan
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -85,8 +92,8 @@ class PostTableViewCell: UITableViewCell {
         authorLabel.text = post.author
         postImageView.image = UIImage(named: post.image)
         descriptionText.text = post.description
-        likesLabel.text = String(post.likes)
-        viewsLabel.text = String(post.views)
+        likesLabel.text = String("  Likes: \(post.likes)")
+        viewsLabel.text = String("Views: \(post.views)")
     }
     
     private func setupViews() {
@@ -94,7 +101,10 @@ class PostTableViewCell: UITableViewCell {
         vStack.addArrangedSubview(authorLabel)
         vStack.addArrangedSubview(postImageView)
         vStack.addArrangedSubview(descriptionText)
+        hStack.addArrangedSubview(likesLabel)
+        hStack.addArrangedSubview(viewsLabel)
         contentView.addSubview(vStack)
+        contentView.addSubview(hStack)
     }
     
     private func setConstraints() {
@@ -109,16 +119,21 @@ class PostTableViewCell: UITableViewCell {
         postImageView.heightAnchor.constraint(equalToConstant: contentView.bounds.width).isActive = true
         postImageView.widthAnchor.constraint(equalToConstant: contentView.bounds.width).isActive = true
         
-        descriptionText.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: 16).isActive = true
+        descriptionText.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: -16).isActive = true
         descriptionText.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0).isActive = true
         descriptionText.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
         descriptionText.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
         
-        
+       
         vStack.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         vStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         vStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
         vStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        hStack.topAnchor.constraint(equalTo: vStack.bottomAnchor, constant: 16).isActive = true
+        hStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
+        hStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0).isActive = true
+        hStack.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
     }
     
