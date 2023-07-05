7f6cdda5206c08a3ea9f3ae6f946839549268b53
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-05 15:37:30 +0200

    implementingCollectionView

--- Navigation/ProfileModule/PhotoTableViewCell.swift
+++ Navigation/ProfileModule/PhotoTableViewCell.swift
@@ -12,18 +12,46 @@ class PhotoTableViewCell: UITableViewCell {
     lazy var collectionView: UICollectionView = {
        
         let collection = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        collection.collectionViewLayout = layout
+        collection.delegate = self
+        collection.dataSource = self
+        
+        //collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+
         return collection
     
     }()
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView(){
+        self.addSubview(collectionView)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+}
 
-        // Configure the view for the selected state
+extension PhotoTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        cell.backgroundColor = .black
+        return cell
+    }
+}
 
+extension PhotoTableViewCell: UICollectionViewDelegateFlowLayout {
+    
+    
 }
