1944a0fd3a49dc945cecf2ce767f1049f7b67b2a
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-05 19:17:29 +0200

    collectionViewEmbededInTableViewCell()

--- Navigation/ProfileModule/PhotoCollectionViewCell.swift
+++ Navigation/ProfileModule/PhotoCollectionViewCell.swift
@@ -10,9 +10,9 @@ import UIKit
 class PhotoCollectionViewCell: UICollectionViewCell {
     
      var view: UIView = {
-       let view = UIView()
+       let view = UIView(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
         view.backgroundColor = .green
-         view.translatesAutoresizingMaskIntoConstraints = false
+         //view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
@@ -20,11 +20,13 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.contentView.addSubview(view)
-        
+        /*
         view.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
         view.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
         view.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
         view.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        
+        */
     }
     
     required init?(coder: NSCoder) {
--- Navigation/ProfileModule/PhotoTableViewCell.swift
+++ Navigation/ProfileModule/PhotoTableViewCell.swift
@@ -9,78 +9,74 @@ import UIKit
 
 class PhotoTableViewCell: UITableViewCell {
 
-    //var collectionView: UICollectionView!
-    
-    private lazy var view: UIView = {
-        let view = UIView(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
-        view.backgroundColor = .green
-        //view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 200, height: 200), collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = .green
+        collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionCell")
+        return collectionView
     }()
+    
+  
       
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        //setupCollectionView()
-        setupView()
+        setupCollectionView()
+       
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupView() {
-        contentView.addSubview(view)
-        
-        
-        
-    }
+
     
-    /*
+    
     private func setupCollectionView() {
         
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: setupFlowLayout())
-        self.addSubview(collectionView)
+        self.contentView.addSubview(collectionView)
         
+        /*
         collectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        */
         
     }
     
-    */
 
-    /*
-    private func setupFlowLayout() -> UICollectionViewFlowLayout {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        return layout
-    }
-     */
+     
 
 }
 
-
-/*
  extension PhotoTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
- 1
+ 2
  }
  
  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
- let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! PhotoCollectionViewCell
- cell.view.backgroundColor = .green
+ let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! PhotoCollectionViewCell
+ //cell.view.backgroundColor = .green
  
  return cell
  }
  }
  
- extension PhotoTableViewCell: UICollectionViewDelegateFlowLayout {
  
- func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
- CGSize(width: 100, height: 100)
- }
  
- }
- 
- */
+/*
+private func setupView() {
+    contentView.addSubview(view)
+}
+*/
+
+/*
+private lazy var view: UIView = {
+    let view = UIView(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+    view.backgroundColor = .green
+    //view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+}()
+
+*/
+
