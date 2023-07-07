ffab95e8173746e637003b6784feeb763812a661
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-07 20:14:39 +0200

    colecctionViewWithPhotos

--- Navigation/PhotoModul/PhotoViewController.swift
+++ Navigation/PhotoModul/PhotoViewController.swift
@@ -16,12 +16,15 @@ class PhotoViewController: UIViewController {
 
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.backgroundColor = .cyan
+        //collectionView.backgroundColor = .cyan
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
+    
+    private var photos = PhotoModel.getPhotos()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -32,7 +35,7 @@ class PhotoViewController: UIViewController {
     private func setupCollectionView() {
         self.view.addSubview(collectionView)
     
-        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor,constant: 0).isActive = true
+        collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 0).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: 0).isActive = true
@@ -43,14 +46,14 @@ class PhotoViewController: UIViewController {
 extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        20
+        photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!
         PhotoCollectionViewCell
         cell.imageView.backgroundColor = .black
-        
+        cell.imageView.image = UIImage(named: photos[indexPath.item].photoName)
         return cell
     }
     
