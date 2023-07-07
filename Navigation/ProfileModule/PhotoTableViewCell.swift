f0d7c052b9548b32a0ef276f8cf17084bc40637d
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-07 17:46:56 +0200

    arrowSet

--- Navigation/ProfileModule/PhotoTableViewCell.swift
+++ Navigation/ProfileModule/PhotoTableViewCell.swift
@@ -31,12 +31,13 @@ class PhotoTableViewCell: UITableViewCell {
         
     }()
     
-    private lazy var arrowLable: UILabel = {
-        let label = UILabel()
-        label.text = "->"
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    private lazy var arrowImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        //imageView.contentMode = .right
+        imageView.image = UIImage(systemName: "arrow.right")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
         
     }()
     
@@ -55,13 +56,13 @@ class PhotoTableViewCell: UITableViewCell {
     private func setupCollectionView() {
         self.contentView.addSubview(collectionView)
         self.contentView.addSubview(collectionTitle)
-        self.contentView.addSubview(arrowLable)
+        self.contentView.addSubview(arrowImage)
             
         
-        arrowLable.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 24).isActive = true
-        arrowLable.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -12).isActive = true
-        arrowLable.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        arrowLable.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2).isActive = true
+        arrowImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 20).isActive = true
+        arrowImage.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: 0).isActive = true
+        arrowImage.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        arrowImage.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 4).isActive = true
         
         collectionTitle.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 24).isActive = true
         collectionTitle.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 12).isActive = true
