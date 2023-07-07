7a6fdd69be2faf2c3d0a280e972beb7ff3795270
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-07-07 20:09:58 +0200

    collectionViewForGallery

--- Navigation/PhotoModul/PhotoViewController.swift
+++ Navigation/PhotoModul/PhotoViewController.swift
@@ -12,10 +12,11 @@ class PhotoViewController: UIViewController {
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
+        layout.scrollDirection = .vertical
 
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = .cyan
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -24,14 +25,14 @@ class PhotoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
     
     
     private func setupCollectionView() {
         self.view.addSubview(collectionView)
     
-        collectionView.topAnchor.constraint(equalTo: self.view.bottomAnchor,constant: 0).isActive = true
+        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor,constant: 0).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor,constant: 0).isActive = true
@@ -48,7 +49,37 @@ extension PhotoViewController: UICollectionViewDelegate, UICollectionViewDataSou
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as!
         PhotoCollectionViewCell
-        //cell.imageView.image = UIImage(named: photos[indexPath.item].photoName)
+        cell.imageView.backgroundColor = .black
+        
         return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let screenWindth = UIScreen.main.bounds.width
+        
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        
+        let interLineSpaces = layout.minimumLineSpacing*4
+        let itemWindth = (screenWindth - interLineSpaces)/3
+        
+        let itemSize = CGSize(width: itemWindth, height: itemWindth)
+        
+        return itemSize
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
 }
